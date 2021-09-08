c40ae90a1cb29cb9c6ca7c03bca32eabab202477
Yuriy Volodin <vjv@inbox.ru>
2021-09-08 06:52:26 +0300

    add test

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+import eyed3
 import pytest as pytest
 
 import youtube2mp3
@@ -28,8 +29,9 @@ def test_get_options():
 def test_process_downloaded_mp3(tmpdir, monkeypatch):
     # prepare test environment
     working_dir = tmpdir.mkdir('working_dir')
-    working_dir.join('test.mp3').write('test')
-    json_file = working_dir.join('test.info.json')
+    working_dir.join('test_1.mp3').write('test')
+    working_dir.join('test_2.mp3').write('test with no json info file')
+    json_file = working_dir.join('test_1.info.json')
     json_file.write(json.dumps({'title': 'test - title.'}))
     monkeypatch.setattr(youtube2mp3, 'set_id3_tag', lambda *args: None)
     os.chdir(working_dir)
@@ -40,8 +42,24 @@ def test_process_downloaded_mp3(tmpdir, monkeypatch):
     assert os.path.exists('test_title.mp3')
 
 
-def test_set_id3_tag():
-    pass
+def test_set_id3_tag(tmpdir):
+    tmp_file = tmpdir.join('downloaded_track.mp3')
+    for_test = os.path.join(os.path.dirname(__file__), 'for_test.mp3')
+    with open(for_test, 'rb') as fd_r, open(tmp_file, 'wb') as fd_w:
+        fd_w.write(fd_r.read())
+    info = {
+        'uploader': 'Andrew Codeman',
+        'upload_date': '20180101',
+        'title': 'Bear with a guitar',
+        'tags': ['CC BY 4.0']
+    }
+    youtube2mp3.set_id3_tag(str(tmp_file), info)
+    audiofile = eyed3.load(tmp_file)
+    assert audiofile.tag.recording_date.year == 2018
+    assert audiofile.tag.title == info['title']
+    assert audiofile.tag.artist == info['uploader']
+    assert audiofile.tag.album == info['uploader']
+    assert audiofile.tag.album_artist == info['tags'][0]
 
 
 def test_download_task_list():
